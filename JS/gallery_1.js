baf811ce19ad38528d8eb9fd60dadc62d659a762
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-12 00:28:45 +0100

    Update gallery_1.js

--- JS/gallery_1.js
+++ JS/gallery_1.js
@@ -28,6 +28,12 @@ for(i = 0; i < rows.length; i++) {
 function openModal(e) {
   var image = e.currentTarget;
 
+  // This assumes the HTML structure is exactly as you posted
+  var modal = picture.parentNode.parentNode.nextElementSibling;
+  
+  // Set the display to flex
+  modal.style.display = "flex";
+
   // Choose a slide
   if(image.hasAttribute("data-number")) {
     var slideNum = image.getAttribute("data-number");
@@ -49,218 +55,34 @@ function nextSlide(e) {
   showSlide(modal, num);
 }
 
-
-
-function openLightbox_1() { 
-  document.getElementById('mouseTarget').style.position = 'fixed';
-  document.getElementById('mouseTarget').style.height = '3.5rem';
-  document.getElementById('mouseTarget').style.top = '0';
-  document.getElementById('mouseTarget').style.zIndex = '100001';
-  document.getElementsByTagName('body')[0].style.overflow ="hidden";
-  document.getElementById('Lightbox_1').style.display = 'flex';
-  document.getElementById('rowid').style.display = 'none';
-  document.getElementById('menu-toggle').style.display = 'none';
-  document.getElementsByTagName("Footer")[0].style.display = "none";
-  document.getElementById('menu-toggle').style.display = 'none';
-  x.removeListener(scale);
-var dots = window.matchMedia( "(min-width: 350px)" );
-if (dots.matches) {
-  document.getElementById('dots1').style.display = 'grid';
+function prevSlide(e) {
+  // This assumes the HTML structure is exactly as you posted
+  var modal = e.currentTarget.parentNode.parentNode;
+  var num = modal.getAttribute("data-slide");
+  if(!num) num = 0;
+  num--;
+  showSlide(modal, num);
 }
-else {
-  document.getElementById('dots1').style.display = 'none';}};
 
+function showSlide(modal, num) {
+  var slides = modal.getElementsByClassName("mySlides");
+  var dots = modal.getElementsByClassName("demo");
+  var captionText = modal.querySelector(".caption p");
+  if (num >= slides.length) { num = 0 }
+  if (num < 0) {num = slides.length - 1}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[num].style.display = "flex";
+  dots[num].className += " active";
+  captionText.innerHTML = dots[num].alt;
 
-function closeLightbox_1() {
-  x.addListener(scale);
-  document.getElementById('mouseTarget').style.position = '';
-  document.getElementById('mouseTarget').style.height = '';
-  document.getElementById('mouseTarget').style.top = '0';
-  document.getElementById('mouseTarget').style.zIndex = '';
-  document.getElementsByTagName('body')[0].style.overflow ="";
-  document.getElementById('Lightbox_1').style.display = 'none'; 
-  document.getElementById('rowid').style.display = 'grid';
-  document.getElementById('menu-toggle').style.display = 'flex';
-  document.getElementsByTagName("Footer")[0].style.display = "flex";
-var mq = window.matchMedia( "(max-width: 950px)" );
-if (mq.matches) {
-  document.getElementById('menu-toggle').style.display = 'flex';
+  // Store the current slide in an attribute
+  modal.setAttribute("data-slide", num);
 }
-else {
-  document.getElementById('menu-toggle').style.display = 'none';}};
-
-
-
-function openLightbox_2() { 
-  document.getElementById('mouseTarget').style.position = 'fixed';
-  document.getElementById('mouseTarget').style.height = '3.5rem';
-  document.getElementById('mouseTarget').style.top = '0';
-  document.getElementById('mouseTarget').style.zIndex = '100001';
-  document.getElementsByTagName('body')[0].style.overflow ="hidden";
-  document.getElementById('Lightbox_2').style.display = 'flex';
-  document.getElementById('rowid').style.display = 'none';
-  document.getElementById('menu-toggle').style.display = 'none';
-  document.getElementsByTagName("Footer")[0].style.display = "none";
-  document.getElementById('menu-toggle').style.display = 'none';
-  x.removeListener(scale);
-var dots = window.matchMedia( "(min-width: 350px)" );
-if (dots.matches) {
-  document.getElementById('dots2').style.display = 'grid';
-}
-else {
-  document.getElementById('dots2').style.display = 'none';}};
-
-
-function closeLightbox_2() {
-  x.addListener(scale);
-  document.getElementById('mouseTarget').style.position = '';
-  document.getElementById('mouseTarget').style.height = '';
-  document.getElementById('mouseTarget').style.top = '0';
-  document.getElementById('mouseTarget').style.zIndex = '';
-  document.getElementsByTagName('body')[0].style.overflow ="";
-  document.getElementById('Lightbox_2').style.display = 'none'; 
-  document.getElementById('rowid').style.display = 'grid';
-  document.getElementById('menu-toggle').style.display = 'flex';
-  document.getElementsByTagName("Footer")[0].style.display = "flex";
-var mq = window.matchMedia( "(max-width: 950px)" );
-if (mq.matches) {
-  document.getElementById('menu-toggle').style.display = 'flex';
-}
-else {
-  document.getElementById('menu-toggle').style.display = 'none';}};
-
-
-function openLightbox_3() { 
-  document.getElementById('mouseTarget').style.position = 'fixed';
-  document.getElementById('mouseTarget').style.height = '3.5rem';
-  document.getElementById('mouseTarget').style.top = '0';
-  document.getElementById('mouseTarget').style.zIndex = '100001';
-  document.getElementsByTagName('body')[0].style.overflow ="hidden";
-  document.getElementById('Lightbox_3').style.display = 'flex';
-  document.getElementById('rowid').style.display = 'none';
-  document.getElementById('menu-toggle').style.display = 'none';
-  document.getElementsByTagName("Footer")[0].style.display = "none";
-  document.getElementById('menu-toggle').style.display = 'none';
-  x.removeListener(scale);
-var dots = window.matchMedia( "(min-width: 350px)" );
-if (dots.matches) {
-  document.getElementById('dots3').style.display = 'grid';
-}
-else {
-  document.getElementById('dots3').style.display = 'none';}};
-
-
-function closeLightbox_3() {
-  x.addListener(scale);
-  document.getElementById('mouseTarget').style.position = '';
-  document.getElementById('mouseTarget').style.height = '';
-  document.getElementById('mouseTarget').style.top = '0';
-  document.getElementById('mouseTarget').style.zIndex = '';
-  document.getElementsByTagName('body')[0].style.overflow ="";
-  document.getElementById('Lightbox_3').style.display = 'none'; 
-  document.getElementById('rowid').style.display = 'grid';
-  document.getElementById('menu-toggle').style.display = 'flex';
-  document.getElementsByTagName("Footer")[0].style.display = "flex";
-var mq = window.matchMedia( "(max-width: 950px)" );
-if (mq.matches) {
-  document.getElementById('menu-toggle').style.display = 'flex';
-}
-else {
-  document.getElementById('menu-toggle').style.display = 'none';}};
-
-
-
-
-
-function changeSlide(a) {
-  showSlide1(slideIndex += a);
-};
-
-function changeSlide(a) {
-  showSlide2(slideIndex += a);
-};
-
-function toSlide1(a) {
-  showSlide1(slideIndex = a);
-};
-
-function showSlide1(a) {
-  const slides1 = document.getElementsByClassName('slide1');
-  let modalPreviews = document.getElementsByClassName('modal-preview');
-  const dot = document.getElementsByClassName('dots2');
-  let dots = document.getElementsByClassName('dot1');
-  
-  if (a > 1) {
-  document.getElementById('again').style.display = 'none';
-  document.getElementById('next').style.display = 'flex';};
-  if (a > slides1.length-1) {
-  document.getElementById('again').style.display = 'flex';
-  document.getElementById('next').style.display = 'none';};
-  if (a > slides1.length) {
-    slideIndex = 1;	
-  document.getElementById('again').style.display = 'none';
-  document.getElementById('next').style.display = 'flex';}
-  if (a < 1) {
-    slideIndex = slides1.length;
-  document.getElementById('again').style.display = 'flex';
-  document.getElementById('next').style.display = 'none';
-  };
-  for (let i = 0; i < slides1.length; i++) {
-    slides1[i].style.display = "none";
-    dots[i].style.background = "black";
-    dots[i].style.opacity = "1";
-    dots[i].style.display = 'grid';};
-  
-  for (let i = 0; i < modalPreviews.length; i++) {
-    modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
-    dot1[i].className = dot[i].className.replace(' active', '');};
-  const dotzz = document.getElementsByClassName('dots');
-  let y = dotzz.length;
-  slides1[slideIndex - 1].style.display = 'flex';
-  dots[slideIndex - 1].style.background = "rgb(219, 10, 10)";
-  dots[slideIndex - 1].style.opacity = ".7";
-  };
-
-function toSlide2(a) {
-  showSlide2(slideIndex = a);
-  };
-
-function showSlide2(a) {
-  const slides2 = document.getElementsByClassName('slide2');
-  let modalPreviews = document.getElementsByClassName('modal-preview');
-  const dot = document.getElementsByClassName('dots');
-  let dots = document.getElementsByClassName('dot2');
-  
-  if (a > 1) {
-  document.getElementById('again').style.display = 'none';
-  document.getElementById('next').style.display = 'flex';};
-  if (a > slides2.length-1) {
-  document.getElementById('again').style.display = 'flex';
-  document.getElementById('next').style.display = 'none';};
-  if (a > slides2.length) {
-    slideIndex = 1;	
-  document.getElementById('again').style.display = 'none';
-  document.getElementById('next').style.display = 'flex';}
-  if (a < 1) {
-    slideIndex = slides2.length;
-  document.getElementById('again').style.display = 'flex';
-  document.getElementById('next').style.display = 'none';
-  };
-  for (let i = 0; i < slides2.length; i++) {
-    slides2[i].style.display = "none";
-    dots[i].style.background = "black";
-    dots[i].style.opacity = "1";
-    dots[i].style.display = 'grid';};
-  
-  for (let i = 0; i < modalPreviews.length; i++) {
-    modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
-    dot2[i].className = dot[i].className.replace(' active', '');};
-  const dotzz = document.getElementsByClassName('dots2');
-  let y = dotzz.length;
-  slides2[slideIndex - 1].style.display = 'flex';
-  dots[slideIndex - 1].style.background = "rgb(219, 10, 10)";
-  dots[slideIndex - 1].style.opacity = ".7";
-  };
 
 
   overlayShown = false,
