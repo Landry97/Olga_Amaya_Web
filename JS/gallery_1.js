8fdd37b48442acd2eda514cf694a242ae33eaffb
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-11 17:22:54 +0100

    Update gallery_1.js

--- JS/gallery_1.js
+++ JS/gallery_1.js
@@ -37,6 +37,54 @@ else {
   document.getElementById('menu-toggle').style.display = 'none';
 }
 };
+
+
+
+
+function openLightbox_2() { 
+  document.getElementById('mouseTarget').style.position = 'fixed';
+  document.getElementById('mouseTarget').style.height = '3.5rem';
+  document.getElementById('mouseTarget').style.top = '0';
+  document.getElementById('mouseTarget').style.zIndex = '100001';
+  document.getElementsByTagName('body')[0].style.overflow ="hidden";
+  document.getElementById('Lightbox_2').style.display = 'flex';
+  document.getElementById('rowid').style.display = 'none';
+  document.getElementById('menu-toggle').style.display = 'none';
+  document.getElementsByTagName("Footer")[0].style.display = "none";
+  document.getElementById('menu-toggle').style.display = 'none';
+  x.removeListener(scale);
+var dots = window.matchMedia( "(min-width: 350px)" );
+if (dots.matches) {
+  document.getElementById('dots').style.display = 'grid';
+}
+else {
+  document.getElementById('dots').style.display = 'none';
+}
+};
+function closeLightbox_2() {
+  x.addListener(scale);
+  document.getElementById('mouseTarget').style.position = '';
+  document.getElementById('mouseTarget').style.height = '';
+  document.getElementById('mouseTarget').style.top = '0';
+  document.getElementById('mouseTarget').style.zIndex = '';
+  document.getElementsByTagName('body')[0].style.overflow ="";
+  document.getElementById('Lightbox_2').style.display = 'none'; 
+  document.getElementById('rowid').style.display = 'grid';
+  document.getElementById('menu-toggle').style.display = 'flex';
+  document.getElementsByTagName("Footer")[0].style.display = "flex";
+var mq = window.matchMedia( "(max-width: 950px)" );
+if (mq.matches) {
+  document.getElementById('menu-toggle').style.display = 'flex';
+}
+else {
+  document.getElementById('menu-toggle').style.display = 'none';
+}
+};
+
+
+
+
+
 function changeSlide(n) {
   showSlide(slideIndex += n);
 };
