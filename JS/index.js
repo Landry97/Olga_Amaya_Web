9ad95e9eb7c7aef614927057b9c3f4a2d9772fdb
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-11 16:14:49 +0100

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -6,13 +6,13 @@ var info4 = document.getElementById('project-info4')
 var info5 = document.getElementById('project-info5')
 var info6 = document.getElementById('project-info6')
 mouseTarget.addEventListener('mouseenter', function(){ 
- info1.style.opacity = "1";
- info2.style.opacity = "1";
- info3.style.opacity = "1";
- info4.style.opacity = "1";
- info5.style.opacity = "1";
- info6.style.opacity = "1";
- document.getElementById("bodyid").style.background = "white";
+ info1.style.opacity = "";
+ info2.style.opacity = "";
+ info3.style.opacity = "";
+ info4.style.opacity = "";
+ info5.style.opacity = "";
+ info6.style.opacity = "";
+ document.getElementById("bodyid").style.background = "black";
  document.getElementById("headerindex").style.color = "white";
 })
 mouseTarget.addEventListener('mouseleave', function(){
