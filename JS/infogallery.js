811b1f44ef295a0fde2e2af3e3064324b6d49790
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-12 22:49:46 +0100

    Delete infogallery.js

--- JS/infogallery.js
+++
@@ -1,6 +0,0 @@
-const infogallery = document.getElementById('infogalleryid');
-const infid = document.getElementById('INFGLLRY');
-infogallery.addEventListener('click', () => {
-      infogallery.classList.toggle('show');
-      infid.classList.toggle('show');
-});
