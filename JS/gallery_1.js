988f7b58186c2186ad7ed08a7074700fe7449bdb
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-16 01:07:03 +0100

    Update gallery_1.js

--- JS/gallery_1.js
+++ JS/gallery_1.js
@@ -407,8 +407,7 @@ function showOverlay() {
 };
 
 var leftKey = 37, upKey = 38, rightKey = 39, downKey = 40;
-var keystate = [100];
-delete keystate[e.keyCode];
+var keystate = [''];
 document.addEventListener("keydown", function (e) {
     keystate[e.keyCode] = true;
 });
