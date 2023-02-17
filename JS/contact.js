620bd0d24b0f6919f183ae4ef976f19470a52a5c
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-17 01:00:01 +0100

    Update contact.js

--- JS/contact.js
+++ JS/contact.js
@@ -1,3 +1,14 @@
 function change_text_headline(){
     document.getElementById("headline").innerHTML = "let's get in contact";
 }
+
+
+var meinIntervall = setInterval(function() { 
+    // alle 3 Sekunden ausführen 
+    update(); 
+    document.getElementById("headline").innerHTML = "let's get in contact";
+    document.getElementById("headline").innerHTML = "nice to meet you";
+}, 15000); 
+
+// Intervall beenden 
+clearInterval(meinIntervall);
