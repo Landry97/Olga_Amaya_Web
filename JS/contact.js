325c98293209049711e865d784419ca3c7c87308
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-17 16:26:34 +0100

    Update contact.js

--- JS/contact.js
+++ JS/contact.js
@@ -1,18 +1,18 @@
 var meinIntervall = setInterval(function() { 
     document.getElementById("headline").innerHTML = "let's get in contact!";
     update(); 
-}, 10000); 
+}, 5001); 
 var meinIntervall2 = setInterval(function() {
     document.getElementById("headline").innerHTML = "International Costume Designer and Fashion Stylist based in Barcelona.";
     update(); 
-}, 20000); 
+}, 10000); 
 
 var meinIntervall3 = setInterval(function() {
     clearInterval(meinIntervall);
     clearInterval(meinIntervall2);
     clearInterval(meinIntervall3);
     update(); 
-}, 39000); 
+}, 10003); 
 
 
 // Intervall beenden 
