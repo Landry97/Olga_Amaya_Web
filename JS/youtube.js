0fe298750698d193d38216b8f82918104bf22f03
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-20 16:44:42 +0100

    Create youtube.js

---
+++ JS/youtube.js
@@ -0,0 +1,9 @@
+function pause() {
+         let video1 = document.getElementById("video-1")
+         video.contentWindow.postMessage( '{"event":"command", "func":"pauseVideo", "args":""}', '*');
+      }
+
+function stop() {
+         let video1 = document.getElementById("video_1")
+         video1.contentWindow.postMessage( '{"event":"command", "func":"stopVideo", "args":""}', '*');
+      }
