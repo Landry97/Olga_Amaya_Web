2125c6aac9f54da4d5e0a12a1fe1663600b26c44
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-20 17:16:33 +0100

    Update youtube.js

--- JS/youtube.js
+++ JS/youtube.js
@@ -1,9 +1,7 @@
-function pause() {
-         let video = document.getElementById("video_1")
-         video.contentWindow.postMessage( '{"event":"command", "func":"pauseVideo", "args":""}', '*');
-      }
-
 function stop() {
-         let video = document.getElementById("video_1")
-         video.contentWindow.postMessage( '{"event":"command", "func":"stopVideo", "args":""}', '*');
-      }
+var frame = document.getElementById("video_1");
+let video = document.getElementById("video_1")
+
+frame.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+video.contentWindow.postMessage( '{"event":"command", "func":"pauseVideo", "args":""}', '*');
+}
