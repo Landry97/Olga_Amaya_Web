8a9eb54619d38076e4d567fb8b60c3bde760df68
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-20 17:51:24 +0100

    Update youtube.js

--- JS/youtube.js
+++ JS/youtube.js
@@ -1,7 +1,19 @@
-function stop() {
-var frame = document.getElementById("video_1");
-let video = document.getElementById("video_1")
+//function stop() {
+//var frame = document.getElementById("video_1");
+//let video = document.getElementById("video_1")
 
-frame.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-video.contentWindow.postMessage( '{"event":"command", "func":"pauseVideo", "args":""}', '*');
-}
+//frame.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+//video.contentWindow.postMessage( '{"event":"command", "func":"pauseVideo", "args":""}', '*');
+//}
+
+
+const videos = document.querySelectorAll('iframe')
+const close = document.querySelector('.close pointer')
+
+close.addEventListener('click', () => {
+   videos.forEach(i => {
+      const source = i.src
+      i.src = ''
+      i.src = source
+   })
+})
