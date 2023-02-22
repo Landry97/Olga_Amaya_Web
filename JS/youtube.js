3f27d2ed2332f0430f8902fc4319f92423edd29f
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-22 20:38:06 +0100

    Update youtube.js

--- JS/youtube.js
+++ JS/youtube.js
@@ -7,28 +7,44 @@
 //}
 
 
-const videos = document.querySelectorAll('iframe')
-var closeIcon = document.getElementsByClassName('close pointer');
-var prevIcon = document.getElementsByClassName('prev'); 
-var nexIcon = document.getElementsByClassName('nex'); 
+//const videos = document.querySelectorAll('iframe')
+//var closeIcon = document.getElementsByClassName('close pointer');
+//var prevIcon = document.getElementsByClassName('prev'); 
+/var nexIcon = document.getElementsByClassName('nex'); 
 
-function closevideo(e)
-{
-    videos.forEach(i => {
-      const source = i.src
-      i.src = ''
-      i.src = source}) 
+//function closevideo(e)
+//{
+//    videos.forEach(i => {
+//      const source = i.src
+//      i.src = ''
+//      i.src = source}) 
+//};
+//
+//for (var i = 0; i < closeIcon.length; i++) {
+//   closeIcon[i].addEventListener('click', closevideo); 
+//}
+//
+//for (var i = 0; i < prevIcon.length; i++) {
+//   prevIcon[i].addEventListener('click', closevideo); 
+//}
+//
+//for (var i = 0; i < nexIcon.length; i++) {
+//   nexIcon[i].addEventListener('click', closevideo); 
+//}
+
+/**
+ * Stop an iframe or HTML5 <video> from playing
+ * @param  {Element} element The element that contains the video
+ */
+var stopVideo = function ( videopause ) {
+	var iframe = element.querySelector( 'iframe');
+	var video = element.querySelector( 'video' );
+	if ( iframe ) {
+		var iframeSrc = iframe.src;
+		iframe.src = iframeSrc;
+	}
+	if ( video ) {
+		video.pause();
+	}
 };
-
-for (var i = 0; i < closeIcon.length; i++) {
-   closeIcon[i].addEventListener('click', closevideo); 
-}
-
-for (var i = 0; i < prevIcon.length; i++) {
-   prevIcon[i].addEventListener('click', closevideo); 
-}
-
-for (var i = 0; i < nexIcon.length; i++) {
-   nexIcon[i].addEventListener('click', closevideo); 
-}
 
