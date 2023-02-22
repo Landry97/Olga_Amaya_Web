fafdb6e0e1a103c701d6e211f9515b2d2c3cca04
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-22 20:47:41 +0100

    Update youtube.js

--- JS/youtube.js
+++ JS/youtube.js
@@ -36,7 +36,7 @@
  * Stop an iframe or HTML5 <video> from playing
  * @param  {Element} element The element that contains the video
  */
-var stopVideo = function ( closevideo ) {
+var stopVideo = function closevideo (e) {
 	var iframe = element.querySelector( 'iframe');
 	var video = element.querySelector( 'video' );
 	if ( iframe ) {
