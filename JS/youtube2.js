0b077917e9a6275aa7ed81548074fa18206d2757
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-23 16:33:22 +0100

    Delete youtube2.js

--- JS/youtube2.js
+++
@@ -1,7 +0,0 @@
-
-
-// to stop the YouTube video
-Video_element.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
-
-// To pause the YouTube video
-Video_element.contentWindow.postMessage('{"event":"command","func":"PauseVideo","args":""}', '*');
