54aafbd108ed5718114c6309a4a12d36af083c7a
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-23 17:44:39 +0100

    Update contact.js

--- JS/contact.js
+++ JS/contact.js
@@ -1,7 +1,7 @@
 var meinIntervall = setInterval(function() { 
     document.getElementById("headline").innerHTML = "let's get in contact!";
 }, 10000); 
-clearInterval((meinIntervall),11000);
+
 var meinIntervall2 = setInterval(function() {
     document.getElementById("headline").innerHTML = "Costume Designer, Art Direction and Fashion Stylist based in Barcelona.";
 }, 20000); 
