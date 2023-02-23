c7f4e020a11a3f2c5fd661958eaf98602a541150
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-23 16:24:37 +0100

    Update youtube2.js

--- JS/youtube2.js
+++ JS/youtube2.js
@@ -1,29 +1,7 @@
-var frame = document.querySelector('iframe');
-var controls = document.querySelectorAll('.arrow right');
 
-var message = function(func) {
-  return JSON.stringify({
-    event: 'command',
-    func: func,
-    args: []
-  });
-};
 
-var execCommand = function(frame) {
-  return function(func) {
-    return function() {
-      frame.contentWindow.postMessage(message(func), '*');
-    };
-  };
-};
+// to stop the YouTube video
+Video_element.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
 
-var frameCommand = execCommand(frame);
-
-var configControl = function(vc) {
-  var func = vc.getAttribute('data-func');
-  var handler = frameCommand(func);
-  vc.tabIndex = 0;
-  vc.addEventListener('click', handler, false);
-};
-
-[].forEach.call(controls, configControl);
+// To pause the YouTube video
+Video_element.contentWindow.postMessage('{"event":"command","func":"PauseVideo","args":""}', '*');
