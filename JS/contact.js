093dbe5f3e812b7b72f9809edcdcb4cf961982c0
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-24 22:50:38 +0100

    Update contact.js

--- JS/contact.js
+++ JS/contact.js
@@ -1,5 +1,5 @@
 var meinIntervall = setInterval(function() { 
-    document.getElementById("headline").innerHTML = "let's get in contact! <br/> "&nbsp;" ";
+    document.getElementById("headline").innerHTML = "let's get in contact!";
 }, 10000); 
 
 var meinIntervall2 = setInterval(function() {
