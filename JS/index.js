72e171c8fe9831df41b460ceb76a1c8327bf99f9
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-02-25 11:12:53 +0100

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,27 +1,22 @@
-var mouseTarget = document.getElementById('mouseTarget')
-var info1 = document.getElementById('project-info1')
-var info2 = document.getElementById('project-info2')
-var info3 = document.getElementById('project-info3')
-var info4 = document.getElementById('project-info4')
-var info5 = document.getElementById('project-info5')
-var info6 = document.getElementById('project-info6')
+var sizer = window.matchMedia( "(min-width: 950px)" );
+var mouseTarget = document.getElementById('mouseTarget');
+
 mouseTarget.addEventListener('mouseenter', function(){ 
- info1.style.opacity = "";
- info2.style.opacity = "";
- info3.style.opacity = "";
- info4.style.opacity = "";
- info5.style.opacity = "";
- info6.style.opacity = "";
+ if (sizer.matches) {
  document.getElementById("bodyid").style.background = "pink";
- document.getElementById("headerindex").style.color = "";
+}
+else {
+ document.getElementById("bodyid").style.background = '';
+}
+};
 })
+
 mouseTarget.addEventListener('mouseleave', function(){
+ if (sizer.matches) {
  document.getElementById("bodyid").style.background = "";
- document.getElementById("headerindex").style.color = "";
- info1.style.opacity = "";
- info2.style.opacity = "";
- info3.style.opacity = "";
- info4.style.opacity = "";
- info5.style.opacity = "";
- info6.style.opacity = "";
+}
+else {
+ document.getElementById("bodyid").style.background = "";
+}
+};
 })
