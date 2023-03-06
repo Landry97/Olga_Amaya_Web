faf4a806cff203fc60788559dd497c77eb35a235
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-03-06 20:31:57 +0100

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -4,6 +4,7 @@ var mouseTarget = document.getElementById('mouseTarget');
 mouseTarget.addEventListener('mouseenter', function(){ 
  if (sizer.matches) {
  document.getElementById("bodyid").style.background = "var(--secondcolor)";
+ document.getElementsByClassname("figure.project-index").style.border = "var(--secondcolor)";
 }
 else {
  document.getElementById("bodyid").style.background = '';
