42100191070ff64ad668ea2802da98547e1dfd4f
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-03-06 20:41:30 +0100

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -7,13 +7,13 @@ mouseTarget.addEventListener('mouseenter', function(){
  if (sizer.matches) {
  document.getElementById("bodyid").style.background = "var(--secondcolor)";
  for (let i = 0; i < collection.length; i++) {
-  collection[i].style.borderColor = "pink";
+  collection[i].style.border = "solid 2px pink";
 }
 }
 else {
  document.getElementById("bodyid").style.background = '';
  for (let i = 0; i < collection.length; i++) {
-  collection[i].style.borderColor = "pink";
+  collection[i].style.border = "solid 2px pink";
 }
 }
 }
@@ -23,13 +23,13 @@ mouseTarget.addEventListener('mouseleave', function(){
  if (sizer.matches) {
  document.getElementById("bodyid").style.background = "";
   for (let i = 0; i < collection.length; i++) {
-  collection[i].style.borderColor = "pink";
+  collection[i].style.border = "solid 2px pink";
 }
 }
 else {
  document.getElementById("bodyid").style.background = "";
  for (let i = 0; i < collection.length; i++) {
-  collection[i].style.borderColor = "pink";
+  collection[i].style.border = "solid 2px pink";
 }
 }
 }
