84d2bc91f6c60c5bbea3a02090b1ecb4f69a3722
Landry97 <92038899+Landry97@users.noreply.github.com>
2023-03-06 20:36:41 +0100

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,20 @@
 var sizer = window.matchMedia( "(min-width: 950px)" );
 var mouseTarget = document.getElementById('mouseTarget');
 
+const collection = document.getElementsByClassName("figure.project-index");
+
 mouseTarget.addEventListener('mouseenter', function(){ 
  if (sizer.matches) {
  document.getElementById("bodyid").style.background = "var(--secondcolor)";
- document.getElementsByClassname("figure.project-index").style.border = "var(--secondcolor)";
+ for (let i = 0; i < collection.length; i++) {
+  collection[i].style.borderColor = "pink";
+}
 }
 else {
  document.getElementById("bodyid").style.background = '';
+ for (let i = 0; i < collection.length; i++) {
+  collection[i].style.borderColor = "";
+}
 }
 }
 );
@@ -15,9 +22,15 @@ else {
 mouseTarget.addEventListener('mouseleave', function(){
  if (sizer.matches) {
  document.getElementById("bodyid").style.background = "";
+  for (let i = 0; i < collection.length; i++) {
+  collection[i].style.borderColor = "";
+}
 }
 else {
  document.getElementById("bodyid").style.background = "";
+ for (let i = 0; i < collection.length; i++) {
+  collection[i].style.borderColor = "";
+}
 }
 }
 );
